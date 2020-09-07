7bfc9d4df8b40de78626473889a13f9c784636bc
KaiAragaki <adamaragaki@gmail.com>
2020-09-07 00:52:13 -0400

    Finish sp_getfile.R

--- R/sp_putfile.R
+++ R/sp_putfile.R
@@ -3,9 +3,14 @@
 #' @param file path to file to be uploaded
 #' @param dest_path path to folder on SharePoint where file is to be uploaded
 #' @param token AzureGraph token - see example.
+#' @param overwrite should the file overwrite that of one with a similar (see Details) name if it exists?
 #'
 #' @return an http response
 #' @export
+#'
+#' @details
+#' SharePoint API does not care about upper versus lower case.
+#' As a matter of good practice, names should be distinct beyond just capitalization.
 #'
 #' @examples
 #'
@@ -16,8 +21,9 @@
 #' me <- gr$get_user("me")
 #' token <- me$token
 #' }
-sp_putfile <- function(file, dest_path, token) {
+sp_putfile <- function(file, dest_path, token, overwrite = F) {
 
+        base_url <- "https://graph.microsoft.com/v1.0/sites/"
 
         # Get file extension
         ext_ind <- regexpr("\\..[^\\.]*$", file, perl = T)
@@ -34,23 +40,33 @@ sp_putfile <- function(file, dest_path, token) {
 
 
         # Get SharePoint ID
-        url    <- paste0("https://graph.microsoft.com/v1.0/sites/", parsed$host, ":/sites/", parsed$site)
+        url    <- paste0(base_url, parsed$host, ":/sites/", parsed$site)
         sp     <- AzureGraph::call_graph_url(token, url, http_verb = "GET")
         sp_id  <- sp$id
 
 
-        # Get Destination Folder ID
-        url       <- paste0("https://graph.microsoft.com/v1.0/sites/", sp_id, "/drive/root:/", parsed$rest)
+        # Get destination folder ID
+        url       <- paste0(base_url, sp_id, "/drive/root:/", parsed$rest)
         folder    <- AzureGraph::call_graph_url(token, url, http_verb = "GET")
         folder_id <- folder$id
 
+        # Check if file of similar name exists
+        if (!overwrite) {
+                url <- paste0(base_url, sp_id, "/drive/items/", folder_id,"/children")
+                response <- AzureGraph::call_graph_url(token = token, url = url) %>%
+                        dplyr::as_tibble() %>%
+                        tidyr::unnest_wider(col = .data$value) %>%
+                        dplyr::mutate(name  = tolower(.data$name)) %>%
+                        dplyr::filter(.data$name == tolower(file_name))
+                if (nrow(response) > 0) stop("\nFile already exists in destination. \nChange filename (case-INsensitive) or set overwrite to TRUE")
+        }
 
-        # Upload File
-        url <- paste0("https://graph.microsoft.com/v1.0/sites/", sp_id, "/drive/items/", folder_id,":/", file_name, ":/content")
-        AzureGraph::call_graph_url(token = token,
-                                   url = url,
-                                   body = httr::upload_file(file),
-                                   encode = mime::guess_type(ext),
+        # Upload file
+        url <- paste0(base_url, sp_id, "/drive/items/", folder_id,":/", file_name, ":/content")
+        AzureGraph::call_graph_url(token     = token,
+                                   url       = url,
+                                   body      = httr::upload_file(file),
+                                   encode    = mime::guess_type(ext),
                                    http_verb = "PUT")
-
 }
+
