4a98946915d4d1a1c45841dbe6580865672882e3
KaiAragaki <adamaragaki@gmail.com>
2021-04-05 15:13:32 -0400

    init commit

---
+++ R/summon_cell_data.R
@@ -0,0 +1,11 @@
+#' Summon cell line data
+#'
+#' @return a DDS containing cell-line RNA expression
+#' @export
+summon_cell_data <- function() {
+        cell_line_data_url <- "https://livejohnshopkins.sharepoint.com/sites/GBCIStorage/Shared%20Documents/Datasets/Cell%20Line%20RNA%20Seq%20Data/thirty-cell-lines-rnaseq/cell-lines_norm_clades.Rds"
+        token <- sharepointr::sharepoint_token()
+        cell_df <- sharepointr::sharepoint_get(cell_line_data_url, token = token) %>%
+                readRDS()
+}
+
