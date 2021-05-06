b85deb8b9528ea6a7289c9261bff00b6e4ed63ba
KaiAragaki <adamaragaki@gmail.com>
2021-05-06 14:25:38 -0400

    add  ... to pass args to pheatmap

--- R/eda_heatmap.R
+++ R/eda_heatmap.R
@@ -5,11 +5,12 @@
 #' @param genes a character vector containing the names of HGCN symbols to be plotted in the heatmap
 #' @param stratifier optional. A character vector specifying a column in colData by which to color samples in the pheatmap.
 #' @param assay the assay number that contains normalized counts
+#' @param ... additional arguments that will be passed to `pheatmap`
 #'
 #' @return a `pheatmap`
 #' @export
 
-eda_heatmap <- function(dds, genes, stratifier = NULL, assay = 2) {
+eda_heatmap <- function(dds, genes, stratifier = NULL, assay = 2, ...) {
         col_annot <- NULL
         if (!is.null(stratifier)) {
                 col_annot <- data.frame(row.names = colnames(dds),
@@ -20,5 +21,5 @@ eda_heatmap <- function(dds, genes, stratifier = NULL, assay = 2) {
 
         filtered_genes <- dds[which(rownames(dds) %in% genes),]
         norm_exp <- SummarizedExperiment::assay(filtered_genes, assay) %>%
-                pheatmap::pheatmap(annotation_col = col_annot, scale = "row", color = green_black_red(10))
+                pheatmap::pheatmap(annotation_col = col_annot, scale = "row", color = green_black_red(10), border_color = NA, ...)
 }
