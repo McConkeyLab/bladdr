73ff9e042efed9cd1b6afb72930249c3b4aff566
KaiAragaki <adamaragaki@gmail.com>
2021-09-21 12:58:32 -0400

    add 2 gene eda option

--- R/eda.R
+++ R/eda.R
@@ -1,29 +1,101 @@
-eda_two_gene <- function(dds, gene1, gene2, color_by_clade = TRUE) {
-        get_gene_ind <- function(gene_name) {
-
-        }
+#' Plot the expression of one or two genes with an optional stratifier
+#'
+#' @param dds A `DESeqDataSet` object
+#' @param genes A character vector of one or two genes.
+#' @param stratifier A name of a column of the `dds` `colData` that will be used
+#'   to distinguish groups - see the dispatched function documentation for more
+#'   details (see details)
+#' @param assay integer. Assay position in the supplied `dds` that corresponds
+#'   to normalized counts.
+#'
+#' @return a ggplot
+#'
+#' @details `eda_n_gene` will dispatch either `eda_one_gene` or `eda_two_gene`
+#'   depending on the length of the `genes` argument.
+#'
+#' @export
+#'
+eda_n_gene <- function(dds, genes, stratifier = NULL, assay = 2) {
+  if (length(genes) == 2) {
+    eda_two_gene(dds, genes[1], genes[2], stratifier, assay)
+  } else if (length(genes) == 1) {
+    eda_one_gene(dds, genes[1], stratifier, assay)
+  } else {
+    stop("No suitable method to plot this number of genes.")
+  }
 }
 
+
+#' Plot the expression of two genes
+#'
+#' @param dds A `DESeqDataSet` object
+#' @param gene_x character. An HGNC symbol, like "KRT14", to be plotted on the
+#'   x-axis
+#' @param gene_y character. An HGNC symbol, like "SRC", to be plotted on the
+#'   y-axis
+#' @param stratifier A name of a column of the `dds` `colData` that will be used
+#'   to color the points of the scatterplot
+#' @param assay integer. Assay position in the supplied `dds` that corresponds
+#'   to normalized counts.
+#'
+#' @return a ggplot
+#' @export
+#'
+eda_two_gene <- function(dds, gene_x, gene_y, stratifier = NULL, assay = 2) {
+
+  if (is.null(stratifier) &
+      "clade" %in% colnames(SummarizedExperiment::colData(dds))) {
+    stratifier <- "clade"
+  }
+
+  gene_inds <- get_gene_index(dds, c(gene_x, gene_y))
+  gene_expr <- get_gene_expression(dds, gene_inds, assay)
+
+  dds |>
+    SummarizedExperiment::colData() |>
+    as.data.frame() |>
+    cbind(gene_expr) |>
+    ggplot2::ggplot(ggplot2::aes(x = .data[[gene_x]],
+                                 y = .data[[gene_y]],
+                                 color = .data[[stratifier]])) +
+    ggplot2::geom_point() +
+    ggplot2::labs(x = gene_x, y = gene_y) +
+    ggrepel::geom_text_repel(aes(label = .data$cell)) +
+    theme_tufte()
+}
 
 #' Plot the expression of one gene across a stratifying argument
 #'
 #' @param dds A `DESeqDataSet` object
 #' @param gene A character vector HGNC Symbol, like "KRT14"
-#' @param stratifier A name of a column of the `dds` `colData` that will be used as the x-axis to stratify gene expression into groups
-#' @param assay The index of the assay that contains the normalized counts. Defaults to `2`.
+#' @param stratifier A name of a column of the `dds` `colData` that will be used
+#'   as the x-axis to stratify gene expression into groups
+#' @param assay The index of the assay that contains the normalized counts.
 #'
 #' @return A ggplot
 #' @export
 #'
 #'
-eda_one_gene <- function(dds, gene, stratifier, assay = 2) {
+eda_one_gene <- function(dds, gene, stratifier = NULL, assay = 2) {
 
-        gene_ind <- get_gene_index(dds, gene)
-        expression <- SummarizedExperiment::assay(dds, assay)[gene_ind,]
-        plotting_data = data.frame(as.data.frame(SummarizedExperiment::colData(dds)), expression = expression)
-        ggplot2::ggplot(plotting_data, ggplot2::aes(x = .data[[stratifier]], y = expression, color = .data[[stratifier]], fill = .data[[stratifier]])) +
-                ggplot2::geom_dotplot(binaxis = "y", binwidth = 0.2, stackdir = "center") +
-                ggplot2::labs(y = gene, x = NULL) +
-                ggrepel::geom_text_repel(aes(label = .data$cell), nudge_x = 0.5) +
+  if (is.null(stratifier) &
+      "clade" %in% colnames(SummarizedExperiment::colData(dds))) {
+    stratifier <- "clade"
+  }
 
+  gene_ind <- get_gene_index(dds, gene)
+  expression <- get_gene_expression(dds, gene_ind, assay)
+
+  dds |>
+    SummarizedExperiment::colData() |>
+    as.data.frame() |>
+    cbind(expression) |>
+    ggplot2::ggplot(ggplot2::aes(x = .data[[stratifier]],
+                                 y = .data[[gene]],
+                                 color = .data[[stratifier]])) +
+    ggplot2::geom_point() +
+    ggplot2::labs(y = gene, x = NULL) +
+    ggrepel::geom_text_repel(aes(label = .data$cell), nudge_x = 0.5) +
+    theme_tufte() +
+    ggplot2::theme(legend.position = "none")
 }
--- R/utils.R
+++ R/utils.R
@@ -1,29 +1,53 @@
-
+#' Get indices of genes by name in a dds
 #' @noRd
-get_gene_index <- function(dds, gene) {
-        ind <- which(rownames(dds) == gene)
-        if(identical(ind, integer(0))) {
-                stop("Gene does not exist in this dataset")
-        }
-        ind
+get_gene_index <- function(dds, genes) {
+  inds <- which(rownames(dds) %in% genes)
+  if(identical(inds, integer(0))) {
+    stop("None of the supplied genes exist in this dataset.")
+  } else if (length(genes) != length(inds)) {
+    found <- rownames(dds)[inds]
+    missing <- setdiff(genes, found)
+    warning("Dataset does not contain gene: ", paste(missing, collapse = ", "), "\n")
+  }
+  inds
 }
 
+#' Get expression of gene by indices in a dds
+#' @return A dataframe where row names are sample names, and column names are
+#'   gene names
+#'
+#' @details Importantly, returns data as a `data.frame` without dropping
+#'   dimensions.
+get_gene_expression <- function(dds, gene_indices, assay) {
+  SummarizedExperiment::assay(dds, assay)[gene_indices, , drop = FALSE] |>
+    t()
+}
+
+
+#' ggplot theme in the style of Edward Tufte
+#'
+#' @param font_size numeric. Size of `element_text` font.
+#'
+#' @return A ggplot theme
+#' @noRd
 theme_tufte <- function(font_size = 30) {
 
-        if (Sys.info()[["sysname"]] == "Windows") {
-                font <- "Gill Sans MT"
-        } else if (Sys.info()[["sysname"]] == "Darwin") {
-                font <- "Gill Sans"
-        }
+  if (Sys.info()[["sysname"]] == "Windows") {
+    font <- "Gill Sans MT"
+  } else if (Sys.info()[["sysname"]] == "Darwin") {
+    font <- "Gill Sans"
+  } else {
+    font <- "Arial"
+  }
 
-        ggplot2::theme(
-                panel.grid = ggplot2::element_blank(),
-                panel.background = ggplot2::element_rect(fill = "#FFFFF8", color = "#CCCCCC"),
-                plot.background = ggplot2::element_rect(fill = "#FFFFF8"),
-                strip.background = ggplot2::element_rect(fill = "#BBBBB0"),
-                legend.background = ggplot2::element_rect(fill = "#FFFFF8"),
-                legend.position = "top",
-                legend.key = ggplot2::element_blank(),
-                text = ggplot2::element_text(family = font, size = font_size)
-        )
+  ggplot2::theme(
+    panel.grid = ggplot2::element_blank(),
+    panel.background = ggplot2::element_rect(fill = "#FFFFF8", color = "#CCCCCC"),
+    plot.background = ggplot2::element_rect(fill = "#FFFFF8"),
+    strip.background = ggplot2::element_rect(fill = "#BBBBB0"),
+    legend.background = ggplot2::element_rect(fill = "#FFFFF8"),
+    legend.position = "top",
+    legend.key = ggplot2::element_blank(),
+    text = ggplot2::element_text(family = font, size = font_size)
+  )
 }
