52387fce1ee9a1850bfd69f4d785631a272f803e
KaiAragaki <adamaragaki@gmail.com>
2021-09-28 22:11:34 -0400

    continue work on headless plan-pcr

--- R/plan_pcr.R
+++ R/plan_pcr.R
@@ -1,17 +1,14 @@
-plan_pcr <- function(data, n_primers, format = c(96, 384), exclude_border = TRUE,
+plan_pcr <- function(data, n_primers, format = 384, exclude_border = TRUE,
                      primer_names = NULL, headless = TRUE, has_names = TRUE,
-                     make_report = FALSE, report_location = tempfile(), report_name =) {
+                     make_report = FALSE, file_path = NULL) {
+
+  data <- tibble::as_tibble(data)
+
+  format <- match.arg(as.character(format), c("96", "384"))
 
   # TODO
-  # Make sure format is one or other - downstream depends on this
-  # Pull out plate template into its own function
-  # Pick up on getting sample names. Note 'has_names' argument. May want to remove later.
-  ## Should probably include ... in middle of long string in both here and bladdr for display purposes.
-  # Need to make sure that if there are no names, names becomes the first column
-  ## Downstream depends on this - assumes second column is conc.
+  # Should probably include ... in middle of long string in both here and plan-pcr for display purposes.
   # Add documentation (previously comments) to their respective functions
-  # Pick up with report naming argument. Should have a sensible default.
-
 
   if (!headless) {
     browseURL("https://kai-a.shinyapps.io/plan-pcr/")
@@ -26,6 +23,12 @@ plan_pcr <- function(data, n_primers, format = c(96, 384), exclude_border = TRUE
   rna_per_well <- 2#uL Vol RNA/well
 
   # Runtime constants ----------------------------------------------------------
+  sample_names <- get_sample_names(data, has_names)
+
+  if (!has_names) {
+    data <- cbind(sample_names, data)
+  }
+
   final_vol <- ((n_primers * reps) + safety_reps) * rna_per_well |> as.integer()
   n_samples <- nrow(data)
   section_area <- reps * (n_samples + ntc)
@@ -35,7 +38,6 @@ plan_pcr <- function(data, n_primers, format = c(96, 384), exclude_border = TRUE
   max_sections_before_flow <- (plate_dims$cols %/% reps) * (plate_dims$rows %/% (n_samples + ntc)) # max_wide * max_tall
   max_sections_after_flow <-((plate_dims$cols %/% reps) * plate_dims$rows) %/% (n_samples + ntc) # (max_wide * n_row) %/% tot_samples
   max_sections_theoretical <- (plate_dims$rows * plate_dims$cols) %/% section_area
-  sample_names <- get_sample_names(data, has_names)
 
   # Checks ---------------------------------------------------------------------
   if(max_sections_theoretical < n_primers) {
@@ -64,20 +66,20 @@ plan_pcr <- function(data, n_primers, format = c(96, 384), exclude_border = TRUE
   plate <- plate |>
     add_sample_names(reps, sample_names) |>
     add_primer_names(pn) |>
-    right_join(full_plate, by = c("col", "row"))
+    dplyr::right_join(full_plate, by = c("col", "row"))
 
   # Sample preparation  --------------------------------------------------------
   sample_prep <- data |>
-    mutate(vol_to_add = final_rna_conc * final_vol / data[[2]]) |>
-    rowwise() |>
-    mutate(dilution_factor = get_best_factor(vol_to_add)) |>
-    ungroup() |>
-    mutate(diluted_concentration = data[[2]] / dilution_factor,
+    dplyr::mutate(vol_to_add = final_rna_conc * final_vol / data[[2]]) |>
+    dplyr::rowwise() |>
+    dplyr::mutate(dilution_factor = get_best_factor(vol_to_add)) |>
+    dplyr::ungroup() |>
+    dplyr::mutate(diluted_concentration = data[[2]] / dilution_factor,
            final_vol = final_vol,
            diluted_rna_to_add = final_rna_conc * final_vol / diluted_concentration,
            water_to_add = final_vol - diluted_rna_to_add) |>
-    select(-vol_to_add) |>
-    relocate(final_vol, .after = last_col())
+    dplyr::select(-vol_to_add) |>
+    dplyr::relocate(final_vol, .after = dplyr::last_col())
 
   # Mastermix Preparation ------------------------------------------------------
   mm <- tibble::tibble(
@@ -85,6 +87,25 @@ plan_pcr <- function(data, n_primers, format = c(96, 384), exclude_border = TRUE
     vol = c(6.25, .625, .5, 3.125) * (n_samples + ntc + 2) * reps
   )
 
+
+
+  if (make_report) {
+    if (missing(file_path)) {
+      file_path <- tempfile(pattern = paste0(Sys.Date(), "_pcr-report_"), fileext = ".html")
+    }
+
+    params <- list(sample_prep = sample_prep,
+                   mm_prep = mm,
+                   plate = plate,
+                   n_primers = n_primers,
+                   primer_names = primer_names,
+                   format = format,
+                   exclude_border = exclude_border)
+
+    rmarkdown::render("./R/pcr_report-template.Rmd", output_file = file_path,
+                      params = params, envir = new.env(parent = globalenv()))
+
+  }
   list(mm, sample_prep, plate)
 }
 
@@ -136,8 +157,8 @@ denote_vlane <- function(plate, plate_dims, reps) {
   wells_per_lane <- plate_dims$rows * reps
   lanes <- rep(1:n_lanes, each = wells_per_lane)
   length(lanes) <- nrow(plate) # Includes 'unusable' dims (ie borders of borderless)
-  arrange(plate, col, desc(row)) |>
-    mutate(lane_v = lanes)
+  dplyr::arrange(plate, col, dplyr::desc(row)) |>
+    dplyr::mutate(lane_v = lanes)
 }
 
 # Horizontal lanes depend on the number of samples
@@ -147,17 +168,17 @@ denote_hlane <- function(plate, plate_dims, n_samples, ntc) {
   wells_per_lane <- plate_dims$cols * total_samples
   lanes <- rep(1:num_lanes, each = wells_per_lane)
   length(lanes) <- nrow(plate)
-  arrange(plate, desc(row), col) |>
-    mutate(lane_h = lanes)
+  dplyr::arrange(plate, dplyr::desc(row), col) |>
+    dplyr::mutate(lane_h = lanes)
 }
 
 flow_lanes <- function(plate, n_primers, n_samples, ntc, reps) {
   total_samples <- n_samples + ntc
   primers <- rep(1:n_primers, each = total_samples * reps)
   length(primers) <- nrow(plate)
-  arrange(plate, lane_v, desc(row)) |>
-    mutate(primer = primers,
-           primer = if_else(primer <= n_primers,
+  dplyr::arrange(plate, lane_v, dplyr::desc(row)) |>
+    dplyr::mutate(primer = primers,
+           primer = dplyr::if_else(primer <= n_primers,
                             primer,
                             NA_integer_),
            available_well = TRUE)
@@ -165,53 +186,53 @@ flow_lanes <- function(plate, n_primers, n_samples, ntc, reps) {
 
 section_lanes <- function(laned_plate, n_primers) {
   laned_plate |>
-    mutate(primer = lane_h * 100 + lane_v) |> # Numerical hierarchy for ordering
-    arrange(primer) |>
-    group_by(primer) |>
-    mutate(primer = if_else(cur_group_id() <= n_primers,
-                            cur_group_id(),
+    dplyr::mutate(primer = lane_h * 100 + lane_v) |> # Numerical hierarchy for ordering
+    dplyr::arrange(primer) |>
+    dplyr::group_by(primer) |>
+    dplyr::mutate(primer = dplyr::if_else(dplyr::cur_group_id() <= n_primers,
+                                          dplyr::cur_group_id(),
                             NA_integer_),
-           available_well = TRUE) |>
-    ungroup()
+                  available_well = TRUE) |>
+    dplyr::ungroup()
 }
 
 
 ## Add names to lanes...
 add_sample_names <- function(plate, reps, sample_names) {
   plate <- plate |>
-    group_by(primer) |>
-    mutate(sample = (row_number() + 2) %/% reps,
-           sample = if_else(!is.na(primer), sample, NA_real_)) |>
-    group_by(sample) |>
-    nest() |>
-    ungroup()
+    dplyr::group_by(primer) |>
+    dplyr::mutate(sample = (dplyr::row_number() + 2) %/% reps,
+           sample = dplyr::if_else(!is.na(primer), sample, NA_real_)) |>
+    dplyr::group_by(sample) |>
+    tidyr::nest() |>
+    dplyr::ungroup()
 
   sample_names <- c(sample_names, "NTC")
   length(sample_names) <- nrow(plate)
 
   plate |>
-    mutate(sample_name = sample_names) |>
-    unnest(cols = data) |>
-    group_by(sample, primer) |>
-    arrange(desc(row), col) |>
-    mutate(sample_name = if_else(!is.na(primer) & row_number() == 2, sample_name, NA_character_),
+    dplyr::mutate(sample_name = sample_names) |>
+    tidyr::unnest(cols = data) |>
+    dplyr::group_by(sample, primer) |>
+    dplyr::arrange(dplyr::desc(row), col) |>
+    dplyr::mutate(sample_name = dplyr::if_else(!is.na(primer) & dplyr::row_number() == 2, sample_name, NA_character_),
            sample = as.factor(sample))
 }
 
 add_primer_names <- function(plate, primer_names) {
   plate <- plate |>
-    group_by(primer) |>
-    nest() |>
-    arrange(primer) |>
-    ungroup()
+    dplyr::group_by(primer) |>
+    tidyr::nest() |>
+    dplyr::arrange(primer) |>
+    dplyr::ungroup()
 
   length(primer_names) <- nrow(plate) # Account for any NAs (that may or may not be present)
 
   plate |>
-    mutate(primer_name = primer_names) |>
-    unnest(cols = data) |>
-    group_by(primer) |>
-    arrange(lane_v, desc(row), col) |>
-    mutate(primer_name = if_else(row_number() == 2, primer_name, NA_character_),
+    dplyr::mutate(primer_name = primer_names) |>
+    tidyr::unnest(cols = data) |>
+    dplyr::group_by(primer) |>
+    dplyr::arrange(lane_v, dplyr::desc(row), col) |>
+    dplyr::mutate(primer_name = dplyr::if_else(dplyr::row_number() == 2, primer_name, NA_character_),
            primer = as.factor(primer))
 }
