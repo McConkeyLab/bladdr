aa686727b1099e41419896c0dc75a195b5eb0816
Kai Aragaki <adamaragaki@gmail.com>
2022-05-25 23:12:01 -0400

    use microsoft365r instead

--- R/summon_cell_data.R
+++ R/summon_cell_data.R
@@ -3,9 +3,10 @@
 #' @return a DDS containing cell-line RNA expression
 #' @export
 summon_cell_data <- function() {
-        cell_line_data_url <- "https://livejohnshopkins.sharepoint.com/sites/GBCIStorage/Shared%20Documents/Datasets/Cell%20Line%20RNA%20Seq%20Data/thirty-cell-lines-rnaseq/cell-lines_norm_clades.Rds"
-        token <- sharepointr::sharepoint_token()
-        cell_df <- sharepointr::sharepoint_get(cell_line_data_url, token = token) |>
-                readRDS()
+  sp <- get_sharepoint_site(site_url = "https://livejohnshopkins.sharepoint.com/sites/GBCIStorage")
+  drive <- sp$get_drive()
+  temp <- tempfile()
+  drive$download_file("Datasets/Cell Line RNA Seq Data/thirty-cell-lines-rnaseq/cell-lines_norm_clades.Rds", dest = temp)
+  readRDS(temp)
 }
 
