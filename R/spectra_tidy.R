0adfecd3968c5762bbc3254f33db06d51352ada4
Kai Aragaki <adamaragaki@gmail.com>
2022-05-25 23:11:28 -0400

    moved to mop

--- R/spectra_tidy.R
+++
@@ -1,8 +0,0 @@
-spectra_tidy <- function(file_path = NULL) {
-
-        if (is.null(file_path)) {
-                file_path <- file.choose()
-        }
-
-        dat <- readxl::read_excel(file_path)
-}
