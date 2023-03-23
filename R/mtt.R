ffb65e8236a3a756d7494e04c39e032f2a16be0a
Kai Aragaki <adamaragaki@gmail.com>
2023-03-23 12:44:00 -0400

    document

--- R/mtt.R
+++ R/mtt.R
@@ -1,28 +1,40 @@
-#' Make a plot from MTT data
+#' Calculate and fit MTT model from absorbance data
 #'
-#' @param x A `data.frame`, `gp`, or `spectramax` object. See Details.
+#' @param x A `data.frame`, `spectramax`, or `gp` object. See Details.
 #' @param ... Arguments passed to their respective methods
 #'
-#' @details If supplied with a `data.frame`, `mtt` will expect...
+#' @details
 #'
-#' Using a `gp` object is a good idea if you have a 'non-standard' plate layout
-#' (standard being each quarter of the plate is a condition)
+#' If supplied with a `data.frame`, `mtt` will expect columns `condition`,
+#' `drug`, `nm562`, and `nm660`.
 #'
-#' @return a `ggplot`
+#' If supplied with a `spectramax` object, everything should be in place.
+#'
+#' Using a `gp` object (NOT CURRENTLY SUPPORTED) is a good idea if you have a
+#' 'non-standard' plate layout (standard being each quarter of the plate is a
+#' condition)
+#'
+#' @return a `tibble`
 #' @export
 mtt_calc <- function(x, ...) {
   UseMethod("mtt_calc")
 }
 
-# Assume there are already condition and drug columns
+#' @param drug_conc a numeric vector containing drug concentrations of the
+#'   conditions, from left to right
+#' @param ic_pct numeric. The %IC desired, where 25 would represent the
+#'   concentration at which growth was reduced by 25% vs baseline
+#'
 #' @export
 #' @rdname mtt_calc
-mtt_calc.data.frame <- function(x, drug_conc, ic_pct = 50, ...) {
-  drug_conc <- sanitize_drug_conc(drug_conc)
+mtt_calc.data.frame <- function(x, ic_pct = 50, ...) {
+  drug_conc <- sanitize_drug_conc(x$drug)
+
+  x$drug <- ifelse(x$drug == 0,  min(drug_conc), x$drug)
 
   x |>
     rm_unassigned_wells() |>
-    subtract_bg_and_get_mean(drug_conc) |>
+    subtract_bg_and_get_mean() |>
     normalize_to_lowest_conc() |>
     fit_mtt(drug_conc, ic_pct)
 }
@@ -31,6 +43,8 @@ mtt_calc.gp <- function(x, ...) {
 
 }
 
+#' @param condition_names What to name each 'sector' of the plate
+#'
 #' @export
 #' @rdname mtt_calc
 mtt_calc.spectramax <- function(x, condition_names, drug_conc, ic_pct = 50, ...) {
@@ -39,9 +53,10 @@ mtt_calc.spectramax <- function(x, condition_names, drug_conc, ic_pct = 50, ...)
   df <- x$data[[1]]$data |>
     gplate::gp_sec("condition", nrow = 4, ncol = 6, labels = condition_names) |>
     gplate::gp_sec("drug", nrow = 4, ncol = 1, labels = drug_conc, advance = FALSE) |>
-    gplate::gp_serve()
+    gplate::gp_serve() |>
+    dplyr::mutate(drug = as.numeric(levels(.data$drug)[.data$drug]))
 
-  mtt_calc(df, drug_conc = drug_conc, ic_pct = ic_pct)
+  mtt_calc(df, ic_pct = ic_pct)
 }
 
 #' Plot MTT results
@@ -57,7 +72,7 @@ mtt_plot <- function(mtt, plot_ics = FALSE) {
   drug_conc <- c(min(mtt$drug), max(mtt$drug))
 
   fit_curve <- mtt |>
-    dplyr::group_by(condition, fit) |>
+    dplyr::group_by(.data$condition, .data$fit) |>
     tidyr::nest() |>
     dplyr::mutate(
       curve = purrr::map(.data$fit, make_curve, drug_conc)
@@ -119,17 +134,15 @@ rm_unassigned_wells <- function(df) {
 
 #' Subtract background and calculate mean per drug and condition
 #'
-#' @param df a `data.frame` containing columns `condition`, `drug`, `nm562`, and `nm660`.
-#' @param drug_conc a numeric vector containing drug concentrations of the
-#'   conditions, from left to right
+#' @param df a `data.frame` containing columns `condition`, `drug`, `nm562`, and
+#'   `nm660`.
 #'
 #' @return a `tibble`
-subtract_bg_and_get_mean <- function(df, drug_conc) {
+subtract_bg_and_get_mean <- function(df) {
   dplyr::group_by(df, .data$condition, .data$drug) |>
     dplyr::mutate(
       diff = .data$nm562 - .data$nm660,
-      mean = mean(.data$diff),
-      drug = as.numeric(levels(.data$drug)[.data$drug]),
+      mean = mean(.data$diff)
     ) |>
     dplyr::ungroup()
 }
@@ -163,7 +176,7 @@ fit_mtt <- function(df, drug_conc, ic_pct) {
 
       ic = purrr::map(.data$fit, get_ic, ic_pct = ic_pct)
     ) |>
-    tidyr::unnest(cols = c(.data$ic, .data$data)) |>
+    tidyr::unnest(cols = c("ic", "data")) |>
     dplyr::ungroup()
 }
 
@@ -233,8 +246,8 @@ get_ic <- function(fit, ic_pct) {
     drc::ED(fit, respLev = ic_pct, display = FALSE) |>
       dplyr::as_tibble() |>
       dplyr::rename(
-        ic_value = .data$Estimate,
-        ic_std_err = .data$`Std. Error`
+        ic_value = "Estimate",
+        ic_std_err = "Std. Error"
       ) |>
       dplyr::mutate(ic_pct = ic_pct)
   } else {
@@ -244,23 +257,28 @@ get_ic <- function(fit, ic_pct) {
 
 #' Convert 0 to a small enough equivalent
 #'
-#' 0 doesn't behave well with the fitting algorithm. This takes the
+#'
+#' @details 0 doesn't behave well with the fitting algorithm. This takes the
 #' user-supplied drug concentrations and converts the 0 value (if it exists) to
 #' a value low enough to approximate 0 without scaring the fitting algorithm too
 #' much. Specifically, it converts 0 to:
 #'
-#' $second_smallest/third_smallest^4$
+#' \eqn{\frac{SecondSmallest}{ThirdSmallest^4}}
 #'
-#' @param drug_conc numeric vector of drug concentrations, in the order
-#'   which they appear in the section (left to right)
+#' @param drug_conc numeric vector of drug concentrations, in the order which
+#'   they appear in the section (left to right)
+#'
+#' @param quiet Should conversion from 0 to some small number be done silently?
 #'
 #' @return a numeric vector of the same length and order as input
-sanitize_drug_conc <- function(drug_conc) {
+sanitize_drug_conc <- function(drug_conc, quiet = FALSE) {
   if (min(drug_conc) == 0) {
     sorted <- unique(sort(drug_conc))
     new_low <- sorted[2]/(sorted[3]^4)
     drug_conc <- ifelse(drug_conc == 0, new_low, drug_conc)
-    message("Lowest drug concentration is 0, converting to ", new_low)
+    if (!quiet) {
+      message("Lowest drug concentration is 0, converting to ", new_low)
+    }
   }
   drug_conc
 }
