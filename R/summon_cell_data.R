cb2154724f80df512dc425deb3ac3cd00608a881
Kai Aragaki <adamaragaki@gmail.com>
2023-07-18 17:52:01 -0400

    cell data has moved to cellebrate

--- R/summon_cell_data.R
+++
@@ -1,8 +0,0 @@
-#' Summon cell line data
-#' @details This function requires access to the GBCI SharePoint.
-#' @return a DDS containing cell-line RNA expression
-#' @export
-summon_cell_data <- function() {
-  get_gbci_file("Datasets/Cell Line RNA Seq Data/thirty-cell-lines-rnaseq/cell-lines_norm_clades.Rds") |>
-    readRDS()
-}
